708c3647da7bae195fa4e7cd89b2098e6e1f32ec
Ismail <ismailtaher21@gmail.com>
2023-10-31 13:50:25 +0530

    added a new repo

---
+++ 8kyu/Merging sorted integer arrays (without duplicates)/main.js
@@ -0,0 +1,13 @@
+//Write a function that merges two sorted arrays into a single one.
+// The arrays only contain integers. Also, the final outcome must be sorted and not have any duplicate.
+
+
+
+function mergeArrays(a, b) {
+  
+    let merge = [...a, ...b].sort()
+    
+    let unique = new Set(merge)
+    
+    return Array.from(unique).sort((a,b)=> a-b)
+}

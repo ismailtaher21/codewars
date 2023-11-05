0bd85f01dc6a66c1aa471e225a4c0d433e5d56fb
Ismail <ismailtaher21@gmail.com>
2023-11-05 21:29:33 +0530

    new repo

---
+++ 8kyu/Grasshopper - Messi goals function/main.js
@@ -0,0 +1,18 @@
+//Messi is a soccer player with goals in three leagues:
+
+//LaLiga
+//Copa del Rey
+//Champions
+//Complete the function to return his total number of goals in all three leagues.
+
+
+
+
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    let result = (laLigaGoals + copaDelReyGoals + championsLeagueGoals)
+   
+    return result
+    
+  }

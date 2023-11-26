5ea30d8a3f2f7dc17f5ab7e5ef4024e7164944c1
Ismail <ismailtaher21@gmail.com>
2023-11-26 22:58:12 +0530

    new repo

---
+++ 8kyu/Multiple of Index/main.js
@@ -0,0 +1,16 @@
+//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+
+
+
+
+function multipleOfIndex(array) {
+    const result = [];
+  for (let i = 0; i < array.length; i++) {
+   if (array[i] % i === 0 || array[i] === 0) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+  
+}

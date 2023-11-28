90e0a47cb872b79e21896bf82c3510daa5610daf
Ismail <ismailtaher21@gmail.com>
2023-11-29 04:04:59 +0530

    new repo

---
+++ 8kyu/Convert a Number to a String!/main.js
@@ -0,0 +1,10 @@
+//We need a function that can transform a number (integer) into a string.
+
+//What ways of achieving this do you know?
+
+
+function numberToString(num) {
+  
+    return num.toString()
+    // Return a string of the number here!
+  }

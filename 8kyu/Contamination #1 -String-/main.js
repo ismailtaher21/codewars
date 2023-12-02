f36741dfaa1e0a53e55b388766fac115a214e136
Ismail <ismailtaher21@gmail.com>
2023-12-03 05:27:08 +0530

    new repo

---
+++ 8kyu/Contamination #1 -String-/main.js
@@ -0,0 +1,15 @@
+//An AI has infected a text with a character!!
+
+//This text is now fully mutated to this character.
+
+//If the text or the character are empty, return an empty string.
+//There will never be a case when both are empty as nothing is going on!!
+
+
+
+
+function contamination(text, char){
+  
+    return char.repeat(text.length)
+    // Code here ;)
+  }

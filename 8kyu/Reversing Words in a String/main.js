4f57d80007f9c5d01492ed492c207f44dd637866
Ismail <ismailtaher21@gmail.com>
2024-01-12 17:43:47 +0530

    new repo

---
+++ 8kyu/Reversing Words in a String/main.js
@@ -0,0 +1,10 @@
+//You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+
+
+
+function reverse(string){
+    //your code here
+    
+    return string.split(" ").reverse().join(" ")
+  }

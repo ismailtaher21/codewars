5659d171460de995084ecc8e8eec9df87383f853
Ismail <ismailtaher21@gmail.com>
2024-01-14 19:10:40 +0530

    new repo

---
+++ 8kyu/The Wide-Mouthed frog!/main.js
@@ -0,0 +1,14 @@
+//Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. 
+//If this one is an alligator (case-insensitive) return small otherwise return wide.
+
+
+
+
+function mouthSize(animal) {
+    // code here
+    if(animal == 'alligator'){
+      return "small"
+    }else{
+      return "wide"
+    }
+  }
